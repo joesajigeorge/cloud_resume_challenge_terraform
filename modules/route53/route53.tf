fc1a92db9750d9d5e8683b9823e000ee972d3c59
Joe George <sajigeorge.joe@gmail.com>
2025-06-28 20:40:26 +1000

    Use exisitng hosted zone

--- modules/route53/route53.tf
+++ modules/route53/route53.tf
@@ -1,12 +1,12 @@
-resource "aws_route53_zone" "primary" {
-  name = var.domain_name
+data "aws_route53_zone" "primary" {
+  name         = var.domain_name
+  private_zone = false
 }
 
 resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = var.domain_name
   type    = "A"
-
   alias {
     name                   = var.cloudfront_domain_name
     zone_id                = var.cloudfront_hosted_zone_id
@@ -15,10 +15,9 @@ resource "aws_route53_record" "root" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = data.aws_route53_zone.primary.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
-
   alias {
     name                   = var.cloudfront_domain_name
     zone_id                = var.cloudfront_hosted_zone_id
